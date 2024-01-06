6f49efaf061379b2273f72ca4c2b137d24972c51
Renan de Aguiar <renan-aguiar98@hotmail.com>
2024-01-06 14:26:27 -0300

    projeto inicial

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if(botao.classList.contains('dashboard__item__button--return')) {
+        alert('Obrigado por devolver o jogo!');
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        alert('Jogo alugado com sucesso!');
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+
+}
